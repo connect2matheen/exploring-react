55249825348379d307135e4ede28f6912f41e266
Mirza Matheen Baig <connect2matheen@gmail.com>
2022-10-17 23:43:15 +0530

    brand link changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className='row shadow'>
       <nav className="navbar navbar-dark navbar-expand-lg bg-danger">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">S@mple</a>
+          <NavLink className="navbar-brand" to="/">S@mple</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
